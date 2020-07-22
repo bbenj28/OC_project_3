af8ec34aa07948e2e6135059685de2a99eb3967d
Benjamin Breton <bbenjaminbreton@gmail.com>
2020-07-22 13:53:08 +0200

    Initial Commit

---
+++ P3_01_Xcode/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  P3_01_Xcode
+//
+//  Created by Benjamin Breton on 22/07/2020.
+//  Copyright © 2020 bb. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
